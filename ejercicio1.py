139b5171ff20dd6007f2d0c164c190094beea02f
michaelfb803 <michaelfb803@gmail.com>
2025-03-28 10:11:59 -0500

    ejercicio1final

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,6 @@
 #Almacenar en una lista 15 números e imprimir cuantos son ceros, negativos, positivos, además imprimir la suma de números positivos y suma de números negativos.
 
+#iniciar variables
 numeros = []
 ceros = 0
 negativos = []
@@ -7,20 +8,26 @@ positivos = []
 #suma_positivos = 0
 #suma_negativos = 0
 
+#for y rango: va hasta el nuemro 15
 for i in range (15):
     numero = float(input(f"ingrese un numero {i+1}: "))
     numeros.append(numero)
-    
+#ceros = []
+#realizar el ciclo for para recorrer la lista    
 for numero in numeros:
     if numero == 0:
-        ceros += 1
+       ceros += 1
+       #ceros.append(numero)
     elif numero < 0:
         negativos.append(numero)
     else:
         positivos.append(numero)
-        
+
+#mostrar el resultado        
 print(f"La cantidad de ceros es: {ceros}")
-print(f"La cantidad de negativos es: {len(negativos)}")
-print(f"La cantidad de positivos es: {len(positivos)}")
+print(f"La cantidad de negativos es:  {len(negativos)}")
+#print(f"La cantidad de negativos es:  {(negativos)}")
+print(f"La cantidad de positivos es:  {len(positivos)}")
+#print(f"La cantidad de positivos es:  {(positivos)}")
 print(f"suma de numeros positivos es: {sum(positivos)}")
 print(f"suma de numeros negativos es: {sum(negativos)}")
